7084e6ee1f982cf590d8c0bc6fbc9e841c530530
noahbibb21 <noahbibb90@gmail.com>
2020-07-08 18:46:29 -0400

    need to fix some issues but seems clean

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom'
 
 const MovieList = props => {
+  const [movies, setMovies] = useState([])
+  useEffect (() => {
+    const getMovies = () => {
+      axios
+      .get('http://localhost:5000/api/movies')
+      .then(response => {
+       setMovies(response.data);
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+    }
+    getMovies();
+  },[]);
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map(movie =>(
+        <Link to = {`/movie/${movie.id}`}>
+          <MovieDetails key = {movie.id} movie = {movie} />
+        </Link>
       ))}
+      {/* {props.movies.map(movie => (
+        <MovieDetails key={movie.id} movie={movie} />
+      ))} */}
     </div>
   );
 }
